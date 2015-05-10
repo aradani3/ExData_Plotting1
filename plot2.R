122063a437ce0997b8ae3dbafea6b3977389b121
Daniel Arad <aradani3@gmail.com>
2015-05-10 12:04:43 +0300

    Update plot2.R
    Added comments

--- plot2.R
+++ plot2.R
@@ -2,20 +2,20 @@
 # file to the working directory
   
   ## Bring datafrom file to hcp data.frame
-  ## make sure sqldf library is loaded
+  library(sqldf) #needed in order to use read.csv.sql
   
   fileName = "household_power_consumption.txt"
   sql = "select * from file where Date in ('1/2/2007','2/2/2007')"
-  hpc <- read.csv.sql(fileName, 
-                      sql = sql, 
-                      eol = "\n", 
-                      header = TRUE, 
-                      sep = ";"
-  )
+  hpc <- read.csv.sql(fileName,       # read from the household_power_consumption.txt file
+                      sql = sql,      # select only data from the first two days of FEB 2007
+                      eol = "\n",     # End of Line
+                      header = TRUE,  # Take the heared as Column names
+                      sep = ";"       # semicolon separated values
+                      )
   
   ## Add datatime column to hpc (as POSIXct)
-  hpc$datetime <- paste(hpc$Date,hpc$Time,sep = " ")
-  hpc$datetime <- as.POSIXct(strptime(hpc$datetime, 
+  hpc$datetime <- paste(hpc$Date,hpc$Time,sep = " ") # concatenate date and time columns into a new column
+  hpc$datetime <- as.POSIXct(strptime(hpc$datetime,  # convert to POSIXct class
                                       format = "%d/%m/%Y %H:%M:%S"
                                       )
                              )
@@ -23,10 +23,10 @@
   
   # Plot2
   main = "Global Active Power"
-  with(hpc,plot(datetime,
-                Global_active_power, 
-                type = "l", 
-                ylab = paste(main, " (Kilowatts)")
+  with(hpc,plot(datetime,             # plot datetime from hpc data.frame
+                Global_active_power,  # against Global_active_power from hpc data.frame
+                type = "l",           # plot as line
+                ylab = paste(main, " (Kilowatts)") # y axis label
                 )
        )
   
@@ -36,4 +36,4 @@
            width = 480,
            height = 480
   )
-  dev.off()
+  dev.off() # don't forget to close the file
