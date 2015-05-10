23171dc78a7ecca4369984b787b3b9507c18f1ec
Daniel Arad <aradani3@gmail.com>
2015-05-10 11:59:28 +0300

    Update plot1.R
    Added comments

--- plot1.R
+++ plot1.R
@@ -6,26 +6,26 @@
   
   fileName = "household_power_consumption.txt"
   sql = "select * from file where Date in ('1/2/2007','2/2/2007')"
-  hpc <- read.csv.sql(fileName, 
-                      sql = sql, 
-                      eol = "\n", 
-                      header = TRUE, 
-                      sep = ";"
+  hpc <- read.csv.sql(fileName,       # read from the household_power_consumption.txt file
+                      sql = sql,      # select only data from the first two days of FEB 2007
+                      eol = "\n",     # End of Line
+                      header = TRUE,  # Take the heared as Column names
+                      sep = ";"       # semicolon separated values
                       )
   
   ## Add datatime column to hpc (as POSIXct)
-  hpc$datetime <- paste(hpc$Date,hpc$Time,sep = " ")
-  hpc$datetime <- as.POSIXct(strptime(hpc$datetime, 
+  hpc$datetime <- paste(hpc$Date,hpc$Time,sep = " ") # concatenate date and time columns into a new column
+  hpc$datetime <- as.POSIXct(strptime(hpc$datetime,  # convert to POSIXct class
                                       format = "%d/%m/%Y %H:%M:%S"
                                       )
                              ) 
   
   ## Plot1
-  main = "Global Active Power"
-  with(hpc,hist(Global_active_power, 
-                col = "red", 
-                main = main, 
-                xlab = paste(main, " (Kilowatts)"),
+  main = "Global Active Power" # Main title
+  with(hpc,hist(Global_active_power, # Histogram of Global_active_power from the hpc data.frame
+                col = "red", # coloured in red
+                main = main, # add title
+                xlab = paste(main, " (Kilowatts)"), # x axis label
                 )
        )
   
@@ -35,4 +35,4 @@
            width = 480,
            height = 480
            )
-  dev.off()
+  dev.off() # don't forget to close the png file
